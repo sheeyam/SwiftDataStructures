24fe0c65f949733b5146c213229c3f131039df98
Sheeyam Shellvacumar <sheeyam.shellvacumar@gmail.com>
2019-04-14 15:44:42 -0500

    Update Stack.swift

--- Stack.swift
+++ Stack.swift
@@ -1,32 +1,54 @@
 import Foundation
 
-class Node {
-    let value:Int
+class Node<T> {
+    let value:T
     var next:Node?
-    init(value: Int){
+    init(value: T){
         self.value = value
     }
 }
 
-class Stack {
-    var top: Node?
-    func push(_ value: Int) {
+class Stack<T> {
+    var top: Node<T>?
+    func push(_ value: T) {
         let oldTop = top
         top = Node(value: value)
         top?.next = oldTop
     }
 
-    func pop() -> Int? {
+    func pop() -> T? {
         let currentTop = top
         top = top?.next
         return currentTop?.value;
     }
+
+     func peek() -> T? {
+        return top?.value;
+    }
 }
 
-let newStack = Stack()
+struct User {
+    let name: String
+    let userName: String
+}
+
+let userStack = Stack<User>()
+let user1 = User(name: "Sheeyam", userName: "@sheeyams")
+let user2 = User(name: "James", userName: "@james")
+userStack.push(user1)
+userStack.push(user2)
+
+let userPopOne = userStack.pop()
+print(userPopOne?.name)
+print(userPopOne?.userName)
+
+let newStack = Stack<Int>()
 newStack.push(1)
 newStack.push(2)
 newStack.push(3)
+
+let peek = newStack.peek()
+print(peek)
 
 let firstPop = newStack.pop()
 print(firstPop)
